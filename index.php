f6213b4c42dea1035e819e72b3f0c62f3736df0f
Neud Tesfay <kingneud55@gmail.com>
2019-10-03 17:57:51 +0800

    AtoC - Official website

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php require_once("assets/includes/head-tag-contents.php");?>
+</head>
+<body>
+
+<?php require_once("assets/includes/design-top.php");?>
+<?php require_once("assets/includes/navigation.php");?>
+
+
+<?php require_once("assets/includes/footer.php");?>
+
+</body>
+</html>
